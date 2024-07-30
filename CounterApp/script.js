f63a477d20138d504440b279f43821479827f481
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2024-07-30 20:07:18 +0530

    30/07

---
+++ CounterApp/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const counterElement = document.getElementById('counter');
+    const incrementButton = document.getElementById('incrementButton')
+    const decrementButton = document.getElementById('decrementButton')
+
+    let count = 0;
+
+    incrementButton.addEventListener('click', () => {
+        count++;
+        counterElement.textContent = count;
+    })
+    decrementButton.addEventListener('click', () => {
+        count--;
+        counterElement.textContent = count;
+    })
+
+}) 
